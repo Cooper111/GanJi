3c5f7b9d326c135610f09a05cf2ab1a4be341361
Gavin <33509618+Cooper111@users.noreply.github.com>
2018-03-16 20:44:23 +0800

    Update pages_parsing.py
    对area字段获取更新，增加了pub_data,cates,url,last_time字段；
    其中，url字段用于断点续传和多线程，last_time用于后续数据分析
    
    后续优化会统计完整的存活时间（即对售出后404的页面计算时间差）

--- pages_parsing.py
+++ pages_parsing.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import time
 import pymongo
 import re
+import datetime
 '''
 爬取第一页
 '''
@@ -18,7 +19,8 @@ headers={
 }
 def get_links(url,pages=1,who_sells='o'):
     url ='{}{}{}/'.format(url,str(who_sells),str(pages))
-
+    # http://bj.ganji.com/ershoubijibendiannao/o3/
+    # o for personal a for merchant
     web_data = requests.get(url,headers=headers)
     soup = BeautifulSoup(web_data.text,'lxml')
     if(soup.find('ul','pageLink')):
@@ -38,25 +40,53 @@ def get_pageInfo(link):
         if(web_data.status_code !=404):
             try:
                 soup = BeautifulSoup(web_data.text,'lxml')
+                '''
                 area_row = soup.select('ul > li:nth-of-type(2) > a:nth-of-type(2)')[0]
                 p = re.compile(r'<[^>]+>',re.S)
                 area = p.sub('',str(area_row))
-                price = soup.select('.f22.fc-orange.f-type')[0].text.strip()
+                '''
+                '''
+                #wrapper > div.content.clearfix > div.leftBox > div:nth-child(2) > div > ul > li:nth-child(2) > a:nth-child(2)
+                #wrapper > div.content.clearfix > div.leftBox > div:nth-child(2) > div > ul > li:nth-child(2) > a:nth-child(3)
+                正则表达式中，“.”的作用是匹配除“\n”以外的任何字符，也就是说，它是在一行中进行匹配。这里的“行”是以“\n”进行区分的。a字符串有每行的末尾有一个“\n”，不过它不可见。
+                如果不使用re.S参数，则只在每一行内进行匹配，如果一行没有，就换下一行重新开始，不会跨行。而使用re.S参数以后，正则表达式会将这个字符串作为一个整体，将“\n”当做一个普通的字符加入到这个字符串中，在整体中进行匹配。
+                '''
 
+                price = soup.select('.f22.fc-orange.f-type')[0].text.strip()
+                row_time = soup.select('.pr-5')[0].text.strip().split(' ')[0][:6]
+                d2 = datetime.datetime.now()
+                pub_time = '2018-' + row_time[0] + row_time[1] + '-' + row_time[3] + row_time[4]
+                d1 = datetime.datetime.strptime(pub_time, '%Y-%m-%d')
+                last_time = d2 - d1
+                s = int(last_time.days)
+                if (s < 0):
+                    s += 365
                 info = {
                     'title':soup.title.text,
-                    'area':area,
-                    'price':price
+                    'area': list(map(lambda x: x.text, soup.select('ul > li:nth-of-type(2) > a')))[-2:],
+                    'price':price,
+                    'pub_date': row_time,
+                    'cates': list(soup.select(' ul > li:nth-of-type(4) > span')[0].stripped_strings)[0],
+                    'url': link,
+                    'last_time':s
                 }
+                '''
+                    'cates': list(soup.select('ul.det-infor > li:nth-of-type(1) > span')[0].stripped_strings),
+                    #wrapper > div.content.clearfix > div.leftBox > div:nth-child(2) > div > ul > li:nth-child(4) > span.fc-orange
+                    
+                '''
                 item_info.insert_one(info)
                 print(info)
             except AttributeError:
-                pass
+                print('fuck1')
             except IndexError:
-                pass
+                print('fuck2')
         else:
-            pass
+            print('fuck3')
     else:
-        print('fuck')
-
-
+        print('fuck4')
+        
+        
+'''
+get_pageInfo("http://bj.ganji.com/ruanjiantushu/32148303880377x.htm")
+'''
