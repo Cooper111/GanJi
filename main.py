83afaa9743ac20956cbf88696a9be8c7f063f2f6
Gavin <33509618+Cooper111@users.noreply.github.com>
2018-03-18 10:07:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from pages_parsing import get_pageInfo
 from get_list import channel_list
 
 def get_all_links_from(channel):
-    for i in range(2):
+    for i in range(1,100):
         get_links(channel,i)
 
 
 
 if __name__ == '__main__':
+
     pool = Pool()
     # pool = Pool(processes=6)
-    pool.map(get_all_links_from,channel_list.split())
+    pool.map(get_all_links_from, channel_list.split())
+
     pool.close()
     pool.join()
